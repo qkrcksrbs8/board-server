3f6cd80a660972ef91578d1fbfaa8b1f9883bcec
changyun <qkrcksrbs8@naver.com>
2022-01-27 20:08:22 -0800

    jpa 이용하여 게시판 글 쓰기, 수정 반영

--- src/main/java/cg/park/boardserver/controllers/BoardController.java
+++ src/main/java/cg/park/boardserver/controllers/BoardController.java
@@ -5,8 +5,7 @@ import cg.park.boardserver.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +18,20 @@ public class BoardController {
     
     @GetMapping("/list")
     public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
-        model.addAttribute("boards", boards);
+        model.addAttribute("boards", boardRepository.findAll());
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        model.addAttribute("board", (id == null)? new Board() : boardRepository.findById(id).orElse(null));
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 
 }
