3da882266800af1e1571c03d223e247cb707f2c0
changyun <qkrcksrbs8@naver.com>
2022-01-28 14:36:21 -0800

    Post, Get, Put, Delete 구현 REST인척 하는 HTTP API REST 아키텍처를 모두 따르지 않음.

---
+++ src/main/java/cg/park/boardserver/controllers/BoardApiController.java
@@ -0,0 +1,53 @@
+package cg.park.boardserver.controllers;
+
+import java.util.List;
+
+import cg.park.boardserver.model.Board;
+import cg.park.boardserver.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository repository;
+
+    @GetMapping("/Boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title
+                    ,@RequestParam(required = false, defaultValue = "") String content) {
+        return (StringUtils.isEmpty(title) && StringUtils.isEmpty(content))? repository.findAll() : repository.findByTitleOrContent(title, content);
+    }
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return repository.save(newBoard);
+    }
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(board -> {
+                    board.setTitle(newBoard.getTitle());
+                    board.setContent(newBoard.getContent());
+                    return repository.save(board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return repository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
