abe567104a3d588092a138d5f875689b9274998f
changyun <qkrcksrbs8@naver.com>
2022-01-28 19:39:45 -0800

    시큐리티 적용 로그인, 로그아웃, 회원가입

---
+++ src/main/java/cg/park/boardserver/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package cg.park.boardserver.repository;
+
+import cg.park.boardserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ src/main/java/cg/park/boardserver/service/UserService.java
@@ -0,0 +1,27 @@
+package cg.park.boardserver.service;
+
+import cg.park.boardserver.model.Role;
+import cg.park.boardserver.model.User;
+import cg.park.boardserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
+        Role role = new Role();
+        role.setId(1l);
+        user.getRoles().add(role);
+        return userRepository.save(user);
+    }
+}
