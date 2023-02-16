3319843eb184d8c5c95efff2f780b57603dcc474
changyun <qkrcksrbs8@naver.com>
2023-02-16 20:43:37 +0900

    validate 클레스 코드 리펙토링

--- src/main/java/cg/park/boardserver/validator/BoardValidator.java
+++ src/main/java/cg/park/boardserver/validator/BoardValidator.java
@@ -15,9 +15,7 @@ public class BoardValidator implements Validator {
 
     @Override
     public void validate(Object obj, Errors errors) {
-        Board b = (Board) obj;
-        if (StringUtils.isEmpty(b.getContent())){
+        if (StringUtils.isEmpty(((Board) obj).getContent()))
             errors.rejectValue("content", "key", "내용을 입력하세요.");
-        }
     }
 }
